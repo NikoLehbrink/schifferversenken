8c04b2330be896b511390798df7602269142bcf1
NikoLehbrink <nikolai.lehbrink@web.de>
2020-07-23 16:25:57 +0200

    added different design for ships, created random ai

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import copy
+import random
+
 class Gameboard(object):
   def __init__(self, width, height, battleships):
     self.width = width
@@ -17,6 +20,7 @@ class Gameboard(object):
           break
     
     self.shots.append(Shot(shot_location, is_hit))
+    return is_hit
 
   def is_game_over(self):
     for b in self.battleships:
@@ -48,10 +52,15 @@ class Battleship(object):
         element = (head[0] + i, head[1])
 
       body.append(element)
-    return Battleship(body)
+    return Battleship(body, head, length, direction)
 
-  def __init__(self, body):
+
+# Todo: which of this params do we actually need?
+  def __init__(self, body, head, length, direction):
     self.body = body
+    self.head = head
+    self.length = length
+    self.direction = direction
     self.hits = [False] * len(body)
 
   def body_index(self, location):
@@ -66,8 +75,11 @@ class Battleship(object):
         return False
     return True
 
+class Player(object):
 
-
+  def __init__(self, name, shot_function):
+    self.name = name
+    self.shot_function = shot_function
 
 def render(game_board, show_battleships=False):
   field_border_top_bottom = "+" + "-" * game_board.width + "+"
@@ -80,8 +92,24 @@ def render(game_board, show_battleships=False):
   if(show_battleships):
     # add the battleships to the board
     for b in game_board.battleships:
-      for x,y in b.body:
-        board[x][y] = "0"
+      # Durch enmuerate macht man counter variable und wenn anfang oder ende des schiffes → mach andere zeichen
+      for i,(x,y) in enumerate(b.body):
+        if b.direction == "N":
+          characters = ("v", "|", "^")
+        if b.direction == "S":
+          characters = ("^", "|", "v")
+        if b.direction == "E":
+          characters = ("<", "-", ">")
+        if b.direction == "W":
+          characters = (">", "-", "<")
+
+        if i == 0:
+          character= characters[0]
+        elif i == len(b.body) - 1:
+          character = characters[2]
+        else:
+          character = characters[1]
+        board[x][y] = character
 
   # Add shots to the board
   for sh in game_board.shots:
@@ -100,46 +128,55 @@ def render(game_board, show_battleships=False):
 
   print(field_border_top_bottom)
 
-def render_battleships(width, height, battleships):
-  field_border_top_bottom = "+" + "-" * width + "+"
-  print(field_border_top_bottom)
 
-  # construct empty board
-  board = []
-  for x in range(width):
-    board.append([None for y in range(height)])
+def get_random_ai_shot(game_board):
+  x = random.randint(0, game_board.width - 1 )
+  y = random.randint(0, game_board.height - 1)
+  return (x,y)
 
-  # add the battleships to the board
-  for b in battleships:
-    for x,y in b.body:
-      board[x][y] = "0"
-
-  for y in range(height):
-    row = []
-    for x in range(width):
-      row.append(board[x][y] or " ")
-    print("|" + "".join(row) + "|")
-
-  print(field_border_top_bottom)
+def get_human_shot(game_board):
+  inp = input("Wo willst du hinschiessen?\n")
+  x_str, y_str = inp.split(",")
+  x = int(x_str)
+  y = int(y_str)
+  return (x,y)
 
 if __name__=="__main__":
+
   battleships = [
-    Battleship.build((1,2), 2, "S"),
-    # Battleship.build((3,4), 4, "W"),
-    # Battleship.build((5,7), 3, "E")
+    Battleship.build((1,0), 3, "S"),
+    Battleship.build((3,4), 4, "N"),
+    Battleship.build((5,7), 3, "E")
   ]
 
-  game_board = Gameboard(10,10,battleships)
+  game_boards = [
+    Gameboard(10,10,battleships),  
+    Gameboard( 10,10,copy.deepcopy(battleships))  
+  ]
+
+  players = [
+    Player("Rob", get_human_shot),
+    Player("Niko", get_random_ai_shot)
+  ]
+  offensive_index = 0
 
   while(True):
-    # ToDo: Bad user input
-    inp = input("Wo willst du hinschiessen?\n")
-    x_str, y_str = inp.split(",")
-    x = int(x_str)
-    y = int(y_str)
-    game_board.take_shot((x,y))
-    render(game_board)
+    # Immer die Zahl, die der Gegenpart nicht ist
+    defensive_index = (offensive_index + 1) % 2
 
-    if game_board.is_game_over():
-      print("You Win")
+    defensive_board = game_boards[defensive_index]
+    offensive_player = players[offensive_index]
+
+    print("%s Yoour Turn!" % offensive_player.name)
+
+    shot_location = offensive_player.shot_function(defensive_board)
+    # ToDo: Bad user input
+
+    x = defensive_board.take_shot(shot_location)
+    render(defensive_board, True)
+
+    if defensive_board.is_game_over():
+      print("%s You Win" % offensive_player.name)
       break
+    if x == False:
+      offensive_index = defensive_index
